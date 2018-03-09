1cd7ade72791ea1be9cd09f79844d79bda9a6b7e
Yuvraj <yuvrajsinh.vala@letsnurture.com>
2018-03-09 16:00:08 +0530

    change file

---
+++ movie.py
@@ -0,0 +1,129 @@
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# from urllib.parse import urlparse, urlencode
+# from urllib.request import urlopen, Request
+# from urllib.error import HTTPError
+
+import json
+import os
+import requests
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "working"
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    
+    res = processRequest(req)
+    
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    
+    return r
+
+def processRequest(req):
+    if req.get("result").get("action") != "yahooWeatherForecast":
+        return {}
+    yql_query = makeYqlQuery(req)
+    baseurl = "http://www.omdbapi.com/?t="+yql_query+"&apikey=51ab6811"
+    re= requests.get(baseurl).content
+    response= json.loads(re)
+    print(response)
+    speech = "Today the weather in " + response['title']
+
+
+print("Response:")
+print(speech)
+
+return {
+    "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+    
+    return re
+        
+        
+        def makeYqlQuery(req):
+            result = req.get("result")
+            parameters = result.get("parameters")
+            city = parameters.get("geo-city")
+            if city is None:
+                return None
+                    
+                    return city
+
+
+#def makeWebhookResult(data):
+#    query = data.get('query')
+#    if query is None:
+#        return {}
+#
+#    result = query.get('results')
+#    if result is None:
+#        return {}
+#
+#    channel = result.get('channel')
+#    if channel is None:
+#        return {}
+#
+#    item = channel.get('item')
+#    location = channel.get('location')
+#    units = channel.get('units')
+#    if (location is None) or (item is None) or (units is None):
+#        return {}
+#
+#    condition = item.get('condition')
+#    if condition is None:
+#        return {}
+#
+#    # print(json.dumps(item, indent=4))
+#
+#    speech = "Today the weather in " + location.get('title')
+#
+#
+#    print("Response:")
+#    print(speech)
+#
+#    return {
+#        "speech": speech,
+#        "displayText": speech,
+#        # "data": data,
+#        # "contextOut": [],
+#        "source": "apiai-weather-webhook-sample"
+#    }
+
+
+if __name__ == '__main__':
+    
+    app.run(debug=True, port=5000, host='0.0.0.0')
+
